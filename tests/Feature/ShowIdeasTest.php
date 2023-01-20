ca728f4e7bec8e75a414961acef9f47320d5e643
Bolorchuluun <bolorchuluun09@gmail.com>
2023-01-20 16:56:40 +0800

    add test for creating ideas

--- tests/Feature/ShowIdeasTest.php
+++ tests/Feature/ShowIdeasTest.php
@@ -98,12 +98,12 @@ class ShowIdeasTest extends TestCase
 
         $response = $this->get('/');
 
-        $response->assertSee($ideaOne->title);
-        $response->assertDontSee($ideaEleven->title);
-
-        $response = $this->get('/?page=2');
         $response->assertSee($ideaEleven->title);
         $response->assertDontSee($ideaOne->title);
+
+        $response = $this->get('/?page=2');
+        $response->assertSee($ideaOne->title);
+        $response->assertDontSee($ideaEleven->title);
     }
 
     /** @test */
